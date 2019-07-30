fac15337265db03e99bfedd34fb3bde8649c6822
jocelyngalvan <53316249+jocelyngalvan@users.noreply.github.com>
2019-07-30 11:16:29 -0700

    Add Esha's work

---
+++ memesmain.js
@@ -0,0 +1,43 @@
+$("#slideshowMeme > div").hide();
+
+setInterval(function() {
+  $('#slideshowMeme > div:first')
+    .fadeOut(3000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshowMeme');
+},  5000);
+
+$("#slideshowMmeme > div").hide();
+
+setInterval(function() {
+  $('#slideshowMmeme > div:first')
+    .fadeOut(3000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshowMmeme');
+},  5000);
+
+$("#slideshowMmmeme > div").hide();
+
+setInterval(function() {
+  $('#slideshowMmmeme > div:first')
+    .fadeOut(3000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshowMmmeme');
+},  5000);
+
+$("#slideshowMmmmeme > div").hide();
+
+setInterval(function() {
+  $('#slideshowMmmmeme > div:first')
+    .fadeOut(3000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshowMmmmeme');
+},  5000);
